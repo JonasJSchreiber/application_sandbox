357abfea88c78c311e79046f0e11a90c3efa9bdc
Jonas Schreiber <jonasjschreiber@gmail.com>
2021-12-27 22:39:15 -0500

    update to quotations

--- src/main/java/com/jonas/ebook/Quotations.java
+++ src/main/java/com/jonas/ebook/Quotations.java
@@ -5,7 +5,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -22,26 +26,30 @@ public class Quotations {
 	private static final char closeDoubleQuote = '”';
 	private static final String candidateCloseQuotePattern = ".*‘[^”]*’[^A-Za-z].*";
 	private static final String candidateUndo = ".*”[^‘\\s]*”.*";
-	private static final String correctionCandidate = ".*“[^”]*“.*";
+	private static final String correctionCandidate = ".*" + closeDoubleQuote + "[^" + openDoubleQuote + "]*" + closeDoubleQuote + ".*";
 	
 	public static String specialProcessing(String s) {
 		if (s.indexOf(openSingleQuote) != -1 && openSingleQuoteBeforeOpenDoubleQuote(s)) {
 			s = preprocess(s);
 			int i = 0;
 			while (s.matches(candidateCloseQuotePattern)) {
+				List<Integer> targets = new ArrayList<>();
+				int target = s.indexOf(closeSingleQuote, s.indexOf(openDoubleQuote));
+				while (target > 0) {
+					targets.add(target);
+					target = s.indexOf(closeSingleQuote, target+1);
+				}
+				if (!targets.isEmpty()) {
+					String finalS = s;
+					targets = targets.stream()
+							.sorted(Comparator.comparing(j -> rankCharacterAccordingToLikelinessToBeASingleQuote(finalS, (int) j)).reversed())
+							.collect(Collectors.toList());
+					target = targets.get(0);
+					if (isAQualifyingClosedSingleQuote(s, target)) s = s.substring(0, target) + closeDoubleQuote + s.substring(target + 1);
+				}
 				i = s.indexOf(closeSingleQuote, i + 1);
 				if (i == -1) {
 					break;
-				}
-				if (isAQualifyingClosedSingleQuote(s, i)) {
-					// is there a better i?
-					int j = s.indexOf(closeSingleQuote, i + 1);
-					if (isAQualifyingClosedSingleQuote(s, j)) {
-						s = s.substring(0, j) + closeDoubleQuote + s.substring(j + 1, s.length());
-					} else {
-						s = s.substring(0, i) + closeDoubleQuote + s.substring(i + 1, s.length());
-					}
-
 				}
 			}
 			int offset = 0;
@@ -59,6 +67,7 @@ public class Quotations {
 				}
 			}
 			s = postprocess(s);
+//			return postprocess2Head(s);
 			return postprocess2(s);
 		}
 		return s;
@@ -81,6 +90,10 @@ public class Quotations {
 				|| (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z');
 	}
 
+	public static boolean isCharacterPrecededByAnHtmlCharacterAndFollowedByACharacter(String s, int i) {
+		return s.charAt(i-1) == '>' && (s.charAt(i + 1) >= 'a'
+				|| s.charAt(i + 1) <= 'z');
+	}
 
 	public static boolean isCharacterPrecededOrFollowedByALowerCaseCharacter(String s, int i) {
 		return s.charAt(i - 1) >= 'a' || s.charAt(i - 1) <= 'z'
@@ -114,7 +127,7 @@ public class Quotations {
 		return s;
 	}
 
-	public static String postprocess2(String s) {
+	public static String postprocess2Head(String s) {
 		if (s.matches(correctionCandidate)) {
 			int target = s.indexOf(closeSingleQuote, s.indexOf(openDoubleQuote));
 			int target2 = s.indexOf(closeSingleQuote, target+1);
@@ -125,6 +138,33 @@ public class Quotations {
 			}
 		}
 		return s;
+	}
+
+	public static String postprocess2(String s) {
+		if (s.matches(correctionCandidate)) {
+			List<Integer> targets = new ArrayList<>();
+			int target = s.indexOf(closeDoubleQuote, s.indexOf(openDoubleQuote));
+			while (target > 0) {
+				targets.add(target);
+				target = s.indexOf(closeDoubleQuote, target+1);
+			}
+			int secondQuote = s.indexOf(openDoubleQuote, s.indexOf(openDoubleQuote) + 1);
+			if (!targets.isEmpty() && secondQuote > 0) {
+				targets = targets.stream()
+						.sorted(Comparator.comparing(i -> rankCharacterAccordingToLikelinessToBeASingleQuote(s, (int) i)).reversed())
+						.collect(Collectors.toList());
+				target = targets.get(0);
+				return s.substring(0, target) + closeSingleQuote + s.substring(target + 1);
+			}
+		}
+		return s;
+	}
+
+	public static Integer rankCharacterAccordingToLikelinessToBeASingleQuote(String s, int i) {
+		if (isCharacterSandwichedByTwoCharacters(s, i)) return 0;
+		if (isCharacterPrecededByAnHtmlCharacterAndFollowedByACharacter(s, i)) return 2;
+		if (isCharacterPrecededOrFollowedByALowerCaseCharacter(s, i)) return 1;
+		return 3;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -165,47 +205,4 @@ public class Quotations {
 		System.out.println("Finished Processing");
 	}
 
-	@Test
-	public void test() {
-		String s = "<p id=\"hea0001722\" class=\"calibre6\">‘I’m not quite sure,’ he said, looking round the room with the curiosity of someone who had never been in a police cell before, despite the fact that he had spent a couple of years brooding over his fraudulent tax activities in the ’Joy. ‘I was in the bank when the Gardaí arrived and, I must admit, when they walked into my office I got a bit of a fright. I thought I was in trouble again! But, no, it was just to tell me that you were being held here and they needed a parent or guardian to be present while they questioned you and I suppose I’m the closest thing to either one of those. How are you anyway, Cyril?’</p>";
-		System.out.println(specialProcessing(s));
-	}
-
-	@Test
-	public void test2() {
-		String s = "<p id=\"tit001877\" class=\"calibre10\">I saw the expression on your face harden as you digested this piece of information. Of course, I hadn’t yet spoken to you about staying on at UEA and had no idea how you might feel about it. But you didn’t pursue the conversation and, when George finally let go of your hand, you leaned down to kiss me on the cheek with a ‘Hello, gorgeous’ that was completely out of character for you.</p>";
-		System.out.println(specialProcessing(s));
-	}
-
-	@Test
-	public void test3() {
-		String s = "<p class=\"indent\">‘At first, perhaps,’ she admitted, laughing and putting a hand over her mouth to contain her mirth. ‘But I shall meet a wealthy man soon enough. A prince, perhaps. And he will fall in love with <a id=\"page29\"></a>me and we will live together in a palace and I will have all the servants that I require and wardrobes filled with beautiful dresses. I’ll wear different jewellery every day – opals, sapphires, rubies, diamonds – and during the season we will dance together in the throne room of the Winter Palace, and everyone will look at me from morning till night and admire me and wish that they could stand in my place.’</p>";
-		System.out.println(specialProcessing(s));
-	}
-
-	@Test
-	public void test4() {
-		String s = "OEBPS/Text/part0002.xhtml";
-		if (s.matches(".*htm.*")) {
-			System.out.println("true");
-		}
-	}
-
-	@Test
-	public void test5() {
-		String s = "<p class=\"indent-para\">‘Given all that, some say calling a false alarm is shameful,’ Commander Ironfist had said. ‘But I say a Blackguard who doesn’t shout a Nine Kill once in their life isn’t working on edge. We protect the most important people in the world. Work on edge.’</p>";
-		System.out.println(specialProcessing(s));
-	}
-
-	@Test
-	public void test6() {
-		String s = "<p class=\"para\">‘But you curse well,’ Christophe says, encouragingly. ‘Perhaps the best I have heard. Better than my father, who as you know was a great robber and feared through his province.’</p>";
-		System.out.println(specialProcessing(s));
-	}
-
-	@Test
-	public void test7() {
-		String s = "<p class=\"indented\">‘I was going to force her to deal, yeah. I didn’t have anything to lose. Then all this shit happened.’ He flapped his hands at the cell. ‘And Lolo – I mean&#160;.&#160;.&#160;.&#160;fuck!’</p>";
-		System.out.println(specialProcessing(s));
-	}
 }
---
+++ src/test/java/com/jonas/ebook/QuotationsTest.java
@@ -0,0 +1,89 @@
+package com.jonas.ebook;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuotationsTest {
+
+    Quotations quotations;
+    @Before
+    public void setup() {
+        this.quotations = new Quotations();    
+    }
+
+    @Test
+    public void test() {
+        String s = "<p id=\"hea0001722\" class=\"calibre6\">‘I’m not quite sure,’ he said, looking round the room with the curiosity of someone who had never been in a police cell before, despite the fact that he had spent a couple of years brooding over his fraudulent tax activities in the ’Joy. ‘I was in the bank when the Gardaí arrived and, I must admit, when they walked into my office I got a bit of a fright. I thought I was in trouble again! But, no, it was just to tell me that you were being held here and they needed a parent or guardian to be present while they questioned you and I suppose I’m the closest thing to either one of those. How are you anyway, Cyril?’</p>";
+        System.out.println(quotations.specialProcessing(s));
+    }
+
+    @Test
+    public void test2() {
+        String s = "<p id=\"tit001877\" class=\"calibre10\">I saw the expression on your face harden as you digested this piece of information. Of course, I hadn’t yet spoken to you about staying on at UEA and had no idea how you might feel about it. But you didn’t pursue the conversation and, when George finally let go of your hand, you leaned down to kiss me on the cheek with a ‘Hello, gorgeous’ that was completely out of character for you.</p>";
+        System.out.println(quotations.specialProcessing(s));
+    }
+
+    @Test
+    public void test3() {
+        String s = "<p class=\"indent\">‘At first, perhaps,’ she admitted, laughing and putting a hand over her mouth to contain her mirth. ‘But I shall meet a wealthy man soon enough. A prince, perhaps. And he will fall in love with <a id=\"page29\"></a>me and we will live together in a palace and I will have all the servants that I require and wardrobes filled with beautiful dresses. I’ll wear different jewellery every day – opals, sapphires, rubies, diamonds – and during the season we will dance together in the throne room of the Winter Palace, and everyone will look at me from morning till night and admire me and wish that they could stand in my place.’</p>";
+        System.out.println(quotations.specialProcessing(s));
+    }
+
+    @Test
+    public void test4() {
+        String s = "OEBPS/Text/part0002.xhtml";
+        if (s.matches(".*htm.*")) {
+            System.out.println("true");
+        }
+    }
+
+    @Test
+    public void test5() {
+        String s = "<p class=\"indent-para\">‘Given all that, some say calling a false alarm is shameful,’ Commander Ironfist had said. ‘But I say a Blackguard who doesn’t shout a Nine Kill once in their life isn’t working on edge. We protect the most important people in the world. Work on edge.’</p>";
+        String expected = "<p class=\"indent-para\">“Given all that, some say calling a false alarm is shameful,” Commander Ironfist had said. “But I say a Blackguard who doesn’t shout a Nine Kill once in their life isn’t working on edge. We protect the most important people in the world. Work on edge.”</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+
+    @Test
+    public void test6() {
+        String s = "<p class=\"para\">‘But you curse well,’ Christophe says, encouragingly. ‘Perhaps the best I have heard. Better than my father, who as you know was a great robber and feared through his province.’</p>";
+        String expected = "<p class=\"para\">“But you curse well,” Christophe says, encouragingly. “Perhaps the best I have heard. Better than my father, who as you know was a great robber and feared through his province.”</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+
+    @Test
+    public void test7() {
+        String s = "<p class=\"indented\">‘I was going to force her to deal, yeah. I didn’t have anything to lose. Then all this shit happened.’ He flapped his hands at the cell. ‘And Lolo – I mean&#160;.&#160;.&#160;.&#160;fuck!’</p>";
+        String expected = "<p class=\"indented\">“I was going to force her to deal, yeah. I didn’t have anything to lose. Then all this shit happened.” He flapped his hands at the cell. “And Lolo – I mean&#160;.&#160;.&#160;.&#160;fuck!”</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+
+    @Test
+    public void test8() {
+        String s = "<p class=\"indented\">The crew had set up an umbilical leading the three metres or so between hulls. It led ‘down’ to the <em class=\"calibre5\">Vulture</em>’s<em class=\"calibre5\"> </em>crew – though its direction was meaningless to the dead spaces of the <em class=\"calibre5\">Gamin</em>. Medvig’s armless tripod frame was already squatting at the <em class=\"calibre5\">Vulture</em> end, a metal assemblage of dull bronze and copper, their cylindrical body dominated by four square openings. Their long head was featureless save for a couple of mismatched yellow lights – humans liked to have something to focus on. Medvig’s ‘hands’ could act as his own personal remotes and he had already sent these small spiderlike assemblages down the pipe and into the freighter to help Olli with the fine work.</p>";
+        String expected = "<p class=\"indented\">The crew had set up an umbilical leading the three metres or so between hulls. It led “down” to the <em class=\"calibre5\">Vulture</em>’s<em class=\"calibre5\"> </em>crew – though its direction was meaningless to the dead spaces of the <em class=\"calibre5\">Gamin</em>. Medvig’s armless tripod frame was already squatting at the <em class=\"calibre5\">Vulture</em> end, a metal assemblage of dull bronze and copper, their cylindrical body dominated by four square openings. Their long head was featureless save for a couple of mismatched yellow lights – humans liked to have something to focus on. Medvig’s “hands” could act as his own personal remotes and he had already sent these small spiderlike assemblages down the pipe and into the freighter to help Olli with the fine work.</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+
+    @Test
+    public void test9() {
+        String s = "<p class=\"indented\">‘All is achieved,’ he said cheerfully to the two station guards. One opened the cell so a Voyenni trooper could pull Idris out; the other ensured Rollo stayed inside. Then they hauled him out to the clerks’ office, where the staff did their best not to notice what was going on.</p>";
+        String expected = "<p class=\"indented\">“All is achieved,” he said cheerfully to the two station guards. One opened the cell so a Voyenni trooper could pull Idris out; the other ensured Rollo stayed inside. Then they hauled him out to the clerks’ office, where the staff did their best not to notice what was going on.</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+
+    @Test
+    public void test10() {
+        String s = "<p class=\"indented\">‘We don’t know that,’ Rollo said hurriedly. Everyone goggled at him, mutely indicating the evidence on the screens. ‘Look, my children,’ he told them, voice shaking, ‘we don’t know. I mean, the Arch&#160;.&#160;.&#160;.&#160;the Architects .&#160;.&#160;.’ His voice went hoarse and whispery as he tried to say the word, as though it might summon them anew. ‘They went <em class=\"calibre5\">somewhere.</em> Maybe the <em class=\"calibre5\">Oumaru</em> stumbled on them there. Maybe they were intruders, they were punished and left here. A warning for us, perhaps. “Steer clear.” But it doesn’t mean they’re <em class=\"calibre5\">back</em>. It doesn’t mean&#160;.&#160;.&#160;.’</p>";
+        String expected = "<p class=\"indented\">“We don’t know that,” Rollo said hurriedly. Everyone goggled at him, mutely indicating the evidence on the screens. “Look, my children,” he told them, voice shaking, “we don’t know. I mean, the Arch&#160;.&#160;.&#160;.&#160;the Architects .&#160;.&#160;.” His voice went hoarse and whispery as he tried to say the word, as though it might summon them anew. “They went <em class=\"calibre5\">somewhere.</em> Maybe the <em class=\"calibre5\">Oumaru</em> stumbled on them there. Maybe they were intruders, they were punished and left here. A warning for us, perhaps. ‘Steer clear.’ But it doesn’t mean they’re <em class=\"calibre5\">back</em>. It doesn’t mean&#160;.&#160;.&#160;.”</p>";
+        s = quotations.specialProcessing(s);
+        assertEquals(expected, s);
+    }
+}
